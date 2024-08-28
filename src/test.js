8db3a9997b41dd0f0f852d8b9d7ea898e1ab8fd3
Khushbu Kumari <56738986+KhushbuKumari21@users.noreply.github.com>
2024-08-28 21:38:50 +0530

    Add files via upload

---
+++ src/test.js
@@ -0,0 +1,9 @@
+// src/test.ts
+import { describe, it, expect } from '@jest/globals';
+
+describe('Sample Test', () => {
+  it('should be a passing test', () => {
+    expect(true).toBe(true);
+  });
+});
+
