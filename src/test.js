75e88457e1bbc46987299ddc0b8f720e134d3deb
Khushbu Kumari <56738986+KhushbuKumari21@users.noreply.github.com>
2024-08-29 00:52:45 +0530

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // src/test.ts
 import { describe, it, expect } from '@jest/globals';
 
@@ -8,14 +8,4 @@ describe('Sample Test', () => {
   });
 });
 
-=======
-// src/test.ts
-import { describe, it, expect } from '@jest/globals';
 
-describe('Sample Test', () => {
-  it('should be a passing test', () => {
-    expect(true).toBe(true);
-  });
-});
-
->>>>>>> origin/main
